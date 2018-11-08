b11b2706a93cd7681710601af7f3f67545d526ef
Allan Wanderley <awallan259@gmail.com>
2018-11-08 01:30:11 -0200

    commit 2

--- src/connection/ParearConexao.java
+++ src/connection/ParearConexao.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package connection;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-/**
- *
- * @author Allan
- */
+
 public class ParearConexao {
     private static MongoClient mongo;
     private static MongoCollection<Document> collection;
--- src/dao/CrudAgenda.java
+++ src/dao/CrudAgenda.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
 
 import bean.Pessoa;
@@ -14,10 +9,8 @@ import com.mongodb.client.result.DeleteResult;
 import connection.ParearConexao;
 import java.util.Arrays;
 import org.bson.Document;
-/**
- *
- * @author Allan
- */
+
+
 public class CrudAgenda {
     
     
